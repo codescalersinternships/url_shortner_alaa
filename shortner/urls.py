6e2e50afdab07439c18b304e5312ec607872722f
alaamahmoud.1223@gmail.com <alaamahmoud.1223@gmail.com>
2022-09-15 13:11:09 +0200

    add go func

--- shortner/urls.py
+++ shortner/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index, name='index'),
-    path('create', views.create, name='create')
-
-]
+    path('', views.index, name='index'),
+    path('create', views.create, name='create'),
+    path('<str:pk>', views.go, name='go')
+]  
